b85dd2c34631b911a50c7cd4fe5f0a7ce6c463e9
Cassian <cassian@godsted.com>
2026-04-29 10:38:35 -0400

    Config fixes

--- CHANGELOG-LATEST.md
+++ CHANGELOG-LATEST.md
@@ -1,3 +1,6 @@
 ### Added
 - Slime and Magma Buckets can now be dispensed.
 - A new mob effect, Slime Time, obtained from throwing slimeballs at entities.
+
+### Fixed
+- Config notices.
--- src/main/java/cc/cassian/slime/ModConfig.java
+++ src/main/java/cc/cassian/slime/ModConfig.java
@@ -4,12 +4,12 @@ import folk.sisby.kaleido.api.WrappedConfig;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 
 public class ModConfig extends WrappedConfig {
-	@Comment("Whether to use 26.2+ bounciness logic. Note that this replaces large parts of collision code, so mod conflicts may emerge.")
-	@Comment("When disabled, only basic bounciness logic is present.")
+	@Comment("Whether to allow bouncing horizontally, based on 26.2+ bounciness logic.")
+	@Comment("Note that this replaces large parts of collision code, so mod conflicts may emerge.")
 	public boolean horizontalBounciness = true;
 
-	@Comment("Whether to use 26.2+ bounciness logic. Note that this replaces large parts of collision code, so mod conflicts may emerge.")
-	@Comment("When disabled, only basic bounciness logic is present.")
+	@Comment("Whether to allow bouncing vertically, based on 26.2+ bounciness logic.")
+	@Comment("Note that this replaces large parts of collision code, so mod conflicts may emerge.")
 	public boolean verticalBounciness = true;
 
 	@Comment("Whether to allow throwing slimeballs.")
